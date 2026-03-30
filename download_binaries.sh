289594d2afac18e87b56e2515c3cf349ba3da741
Egor Kin <egorkin@gmail.com>
2026-03-30 21:49:29 +0300

    Update download_binaries.sh
    mtg up to 2.2.6

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="2.2.3"
+VERSION="2.2.6"
 BASE_URL="https://github.com/9seconds/mtg/releases/download/v${VERSION}"
 
 echo "=== Downloading mtg v${VERSION} binaries for Android ==="
